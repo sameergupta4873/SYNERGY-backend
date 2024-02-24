231bf26bade5afcaeb310c438fafcd38862adef8
Manav <manavshah.2003.ms@gmail.com>
2024-02-24 18:41:06 +0530

    Backend continuing

--- backend/routes/cexRoute.js
+++ backend/routes/cexRoute.js
@@ -1,8 +1,12 @@
-const {getMarketData,getTransactions} =require('../controllers/cexController')
+const {getMarketData,getTransactions, detectAnomaly, filterTransactions} =require('../controllers/cexController')
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const express = require("express");
 const router = express.Router();
 
 router.route("/getBinance").get(getMarketData);
 router.route("/getTransactions").get(getTransactions);
+router.route("/detectAnomaly").get(detectAnomaly);
+// filterTransactions
+router.route("/filterTransactions").get(filterTransactions);
+
 module.exports = router;
