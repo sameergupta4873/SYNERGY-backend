009e8948bd4f28bb6047761612ecc675f2cb1ffe
sameerishaare <sameergupta4873@gmail.com>
2024-02-25 11:26:36 +0530

    final

--- backend/routes/cexRoute.js
+++ backend/routes/cexRoute.js
@@ -1,12 +1,13 @@
-const {getMarketData,getTransactions, detectAnomaly, filterTransactions, showTransitiveTransactions} =require('../controllers/cexController')
+const {getMarketData,getTransactions, detectAnomaly, filterTransactions, showTransitiveTransactions, getAnamolyTransactions} =require('../controllers/cexController')
 const { isAuthenticatedUser, authorizeRoles } = require("../middleware/auth");
 const express = require("express");
 const router = express.Router();
 
 router.route("/getBinance").get(getMarketData);
 router.route("/getTransactions").post(getTransactions);
+router.route("/getAnamolyTransactions").post(getAnamolyTransactions);
 router.route("/detectAnomaly").post(detectAnomaly);
 // filterTransactions
-router.route("/filterTransactions").get(filterTransactions);
+router.route("/filterTransactions").post(filterTransactions);
 router.route("/showTransitiveTransactions").get(showTransitiveTransactions);
 module.exports = router;
